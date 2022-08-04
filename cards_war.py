3e2e9bcd1512639152bc4d8b6452f2d57f531e78
Shaurya Kajiwala <kajiwalashaurya29@gmail.com>
2022-08-04 14:58:56 +0530

    third commit

--- cards_war.py
+++ cards_war.py
@@ -1,5 +1,4 @@
 import random
-from this import d
 
 class Card():
     def __init__(self,suit,number):
@@ -59,7 +58,6 @@ class Player():
     def get_a_card_from_player1(self):
         temp_card=self.player1_deck[0]
         self.player1_deck.pop(0)
-        print(len(self.player1_deck))
         return temp_card
     
     def get_four_cards_from_player1(self):
@@ -69,22 +67,19 @@ class Player():
         
     
     def add_card_to_player1_deck(self,card):
-        self.player1_deck.append(card)
-        print(len(self.player1_deck))
+        self.player1_deck.extend(card)
+     
     
     def get_player1_deck_length(self):
-        return len(self.player1_deck)
-        
+        return len(self.player1_deck)    
     
     def get_a_card_from_player2(self):
         temp_card=self.player2_deck[0]
         self.player2_deck.pop(0)
-        print(len(self.player2_deck))
         return temp_card
     
     def add_card_to_player2_deck(self,card):
-        self.player2_deck.append(card)
-        print(len(self.player2_deck))
+        self.player2_deck.extend(card)
 
     def get_four_cards_from_player2(self):
         temp_cards = self.player2_deck[0:4]
@@ -93,6 +88,12 @@ class Player():
     
     def get_player2_deck_length(self):
         return len(self.player2_deck)
+    
+    def comapre_two_cards(self):
+        if(self.player1_deck[0].number==self.player2_deck[0].number):
+            return True
+        else:
+            return False
     
         
     
@@ -104,44 +105,99 @@ class Player():
     
 class GameLogic():
     
-    def declare_war(self,player):
-        player1_4_cards = player.get_four_cards_from_player1()
-        player2_4_cards = player.get_four_cards_from_player2()
-
-        while player1_4_cards[len(player1_4_cards)-1] == player2_4_cards[len(player1_4_cards)-1]:
-            if(player.get_player1_deck_length() >= 4 and player.get_player2_deck_length()>=4):
-                player1_4_cards.append(player.get_four_cards_from_player1())
-                player2_4_cards.append(player.get_four_cards_from_player2())
-            else:
-                if(player.get_player1_deck_length() > player.get_player2_deck_length()):
-                    print('player 1 wins')
-                else:
-                    print('player 2 wins')
-        if(player1_4_cards[len(player1_4_cards)-1] > player2_4_cards[len(player1_4_cards)-1]):
-            player.add_card_to_player1_deck([player1_4_cards,player2_4_cards])
+    
+    
+    def check_if_deck_is_over(self, player):
+        if (player.get_player1_deck_length() == 0 or player.get_player2_deck_length() == 0):
+            return True
         else:
-            player.add_card_to_player2_deck([player1_4_cards, player2_4_cards])
+            return False
 
 
-   
-    
+
+    def check_who_wins(self,player):
+        if(player.get_player1_deck_length() > player.get_player2_deck_length()):
+            print('player 1 wins')
+            
+        else:
+            print('player 2 wins')
+            
+    def declare_war(self,player):
+        if(player.get_player1_deck_length() >= 4 and player.get_player2_deck_length() >=4):
+            self.player1_4_cards = player.get_four_cards_from_player1()
+            self.player2_4_cards = player.get_four_cards_from_player2()
+            
+          
+            
+            while (self.player1_4_cards[len(self.player1_4_cards)-1].number == self.player2_4_cards[len(self.player2_4_cards)-1].number):
+                if(player.get_player1_deck_length() >= 4 and player.get_player2_deck_length() >= 4):
+                    self.player1_4_cards.extend(player.get_four_cards_from_player1())
+                    self. player2_4_cards.extend(player.get_four_cards_from_player2())
+                else:
+                    if(player.get_player1_deck_length() > player.get_player2_deck_length()):
+                        print('player 1 wins')
+                        return False
+                    else:
+                        print('player 2 wins')
+                        return False
+         
+            if(self.player1_4_cards[len(self.player1_4_cards)-1].number > self.player2_4_cards[len(self.player2_4_cards)-1].number):
+                cards = self.player1_4_cards+self.player2_4_cards
+                player.add_card_to_player1_deck(cards)
+                return True
+            else:
+                cards = self.player1_4_cards + self.player2_4_cards
+                player.add_card_to_player2_deck(cards)
+                return True 
+        else:
+            self.check_who_wins(player)
+            return False
+
+        
     def compare_players_card(self,player):
-        player1card = player.get_a_card_from_player1()
-        player2card = player.get_a_card_from_player2()
-        print(player1card)
-        print(player2card)
-        if(player1card.number == player2card.number):
-            self.declare_war(player)
+        
+        self.player1card = None
+        self.player2card = None
+        self.player1_deck_length = player.get_player1_deck_length()
+        self.player2_deck_length = player.get_player2_deck_length()
+        
+        
+        while True:
+            if(player.comapre_two_cards()):
+                continue_game=self.declare_war(player)
+                if(continue_game):
+                    if(self.check_if_deck_is_over(player)):
+                        self.check_who_wins(player)
+                        break
+                    else:
+                        continue
+                else:
+                    break
+                
+            self.player1card = player.get_a_card_from_player1()
+            self.player2card = player.get_a_card_from_player2()
             
-        elif(player1card.number > player2card.number  ):
-            player.add_card_to_player1_deck([player1card,player2card])
-            
-        elif(player2card.number > player1card.number):
-            player.add_card_to_player2_deck([player1card,player2card])
-    
+            print(self.player1card)
+            print(self.player2card)
         
             
-    
+            if(self.player1card.number > self.player2card.number):
+                cards=[self.player1card,self.player2card]
+                player.add_card_to_player1_deck(cards);
+
+            elif(self.player2card.number > self.player1card.number):
+                cards = [self.player1card, self.player2card]
+                player.add_card_to_player2_deck(cards)
+                
+            self.player1_deck_length = player.get_player1_deck_length()
+            self.player2_deck_length = player.get_player2_deck_length()
+            
+            print('player 1 len::{}'.format(self.player1_deck_length))
+            print('player 2 len::{}'.format(self.player2_deck_length))
+                
+            if(self.check_if_deck_is_over(player)):
+                self.check_who_wins(player)
+                break
     
 if __name__ == '__main__':
     deck=Deck()
@@ -150,24 +206,4 @@ if __name__ == '__main__':
     player.get_deck_and_divide(deck.get_deck_of_card())
     gml=GameLogic()
     gml.compare_players_card(player) 
-    
-    
-    
-    
-    
-    
-
-            
-        
-        
-
-
-
-
-
-       
-
-       
-        
-
     
